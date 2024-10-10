c2bb0ea86806326526c98ea5d5a06b22adb669c2
mennamohamed0207 <menamohamed0207@gmail.com>
2024-10-10 20:35:17 +0300

    Finish forth problem

--- Problem Set 0/locator.py
+++ Problem Set 0/locator.py
@@ -8,5 +8,9 @@ def locate(grid: Grid, item: Any) -> Set[Tuple[int,int]]:
     It should return a list of (x, y) coordinates that specify the locations that contain the given item
     To know how to use the Grid class, see the file "grid.py"  
     '''
-    #TODO: ADD YOUR CODE HERE
-    utils.NotImplemented()
+    result = set()
+    for y in range(grid.height):
+        for x in range(grid.width):
+            if grid[x, y] == item:
+                result.add((x, y))
+    return result
