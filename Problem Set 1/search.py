d23bde19c046ede2d166f6a396f5738f97f2f95f
mennamohamed0207 <menamohamed0207@gmail.com>
2024-10-28 13:29:17 +0300

    Adding DFS with 3/8

--- Problem Set 1/search.py
+++ Problem Set 1/search.py
@@ -1,9 +1,8 @@
-import queue
 from problem import HeuristicFunction, Problem, S, A, Solution
-from collections import deque
 from helpers import utils
 
 #TODO: Import any modules you want to use
+import queue
 
 # All search functions take a problem and a state
 # If it is an informed search function, it will also receive a heuristic function
@@ -46,8 +45,34 @@ def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
     return None
 
 def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
-    #TODO: ADD YOUR CODE HERE
-    utils.NotImplemented()
+    node = Node(initial_state, [])
+    if problem.is_goal(node.state):
+        return node.cumlative_successful_path
+
+    # Use LifoQueue to implement a stack
+    frontier = queue.LifoQueue()
+    frontier_handle_duplicates = set()
+    frontier.put(Node(initial_state, []))
+    frontier_handle_duplicates.add(node.state)
+
+    explored = set()
+    
+    while not frontier.empty():
+        node = frontier.get()
+        explored.add(node.state)
+
+        for action in problem.get_actions(node.state):
+            child_state = problem.get_successor(node.state, action)
+
+            if child_state not in explored and child_state not in frontier_handle_duplicates:
+                if problem.is_goal(child_state):
+                    return node.cumlative_successful_path + [action]
+                
+                # Push the child node onto the stack (LifoQueue)
+                frontier.put(Node(child_state, node.cumlative_successful_path + [action]))
+                frontier_handle_duplicates.add(child_state)
+
+    return None
     
 def UniformCostSearch(problem: Problem[S, A], initial_state: S) -> Solution:
     #TODO: ADD YOUR CODE HERE
