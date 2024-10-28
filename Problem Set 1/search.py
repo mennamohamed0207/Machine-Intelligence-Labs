d1311d067164b5947d0f2bc0bf1ef2d3b0872630
mennamohamed0207 <menamohamed0207@gmail.com>
2024-10-28 13:18:04 +0300

    Finished BSF

---
+++ Problem Set 1/search.py
@@ -0,0 +1,62 @@
+import queue
+from problem import HeuristicFunction, Problem, S, A, Solution
+from collections import deque
+from helpers import utils
+
+#TODO: Import any modules you want to use
+
+# All search functions take a problem and a state
+# If it is an informed search function, it will also receive a heuristic function
+# S and A are used for generic typing where S represents the state type and A represents the action type
+
+# All the search functions should return one of two possible type:
+# 1. A list of actions which represent the path from the initial state to the final state
+# 2. None if there is no solution
+class Node:
+    state: S
+    cumlative_successful_path:list
+    cost:float
+    def __init__(self,state,cumlative_successful_path,cost=0):
+        self.state=state
+        self.cumlative_successful_path=cumlative_successful_path
+        self.cost=cost
+    
+def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
+    #TODO: ADD YOUR CODE HERE
+    node=Node(initial_state,[])
+    if problem.is_goal(node.state):
+        return node.cumlative_successful_path
+    frontier = queue.Queue()
+    frontier_handle_duplicates=set()
+    frontier.put(Node(initial_state,[]))
+    #this set is added as we are implementing the graph version of the algorithm so we need to handle duplicates states that enters the frontier
+    frontier_handle_duplicates.add(node.state)
+    explored = set()
+    while not frontier.empty():
+        node = frontier.get()
+        explored.add(node)
+        for action in problem.get_actions(node.state):
+            child=problem.get_successor(node.state,action)
+            
+            if child not in explored and child not in frontier_handle_duplicates :
+                if problem.is_goal(child):
+                    return node.cumlative_successful_path+[action]
+                frontier.put(Node(child,node.cumlative_successful_path+[action]))
+                frontier_handle_duplicates.add(child)
+    return None
+
+def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
+    #TODO: ADD YOUR CODE HERE
+    utils.NotImplemented()
+    
+def UniformCostSearch(problem: Problem[S, A], initial_state: S) -> Solution:
+    #TODO: ADD YOUR CODE HERE
+    utils.NotImplemented()
+
+def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFunction) -> Solution:
+    #TODO: ADD YOUR CODE HERE
+    utils.NotImplemented()
+
+def BestFirstSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFunction) -> Solution:
+    #TODO: ADD YOUR CODE HERE
+    utils.NotImplemented()
