4a7d9edd3f02c6e7b437882d3dcc1aa996294719
mennamohamed0207 <menamohamed0207@gmail.com>
2024-10-30 02:06:07 +0300

    Adding UCS

--- Problem Set 1/search.py
+++ Problem Set 1/search.py
@@ -19,15 +19,25 @@ class Node:
         self.state=state
         self.cumlative_successful_path=cumlative_successful_path
         self.cost=cost
+    def __lt__(self, other):
+        return self.cost < other.cost
+    def __gt__(self, other):
+        if self.cost > other.cost:
+            return True
+        if self.cost == other.cost:
+            return self.order > other.order
+        return False
+    def __eq__(self, other):
+        return self.state == other.state
     
-def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
+def BreadthFirstSearch(problem: Problem[S, A], initial: S) -> Solution:
     #TODO: ADD YOUR CODE HERE
-    node=Node(initial_state,[])
+    node=Node(initial,[])
     if problem.is_goal(node.state):
         return node.cumlative_successful_path
     frontier = queue.Queue()
     frontier_handle_duplicates=set()
-    frontier.put(Node(initial_state,[]))
+    frontier.put(Node(initial,[]))
     #this set is added as we are implementing the graph version of the algorithm so we need to handle duplicates states that enters the frontier
     frontier_handle_duplicates.add(node.state)
     explored = set()
@@ -44,13 +54,13 @@ def BreadthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
                 frontier_handle_duplicates.add(child)
     return None
 
-def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
-    node = Node(initial_state, [])
+def DepthFirstSearch(problem: Problem[S, A], initial: S) -> Solution:
+    node = Node(initial, [])
 
 
     frontier = queue.LifoQueue()
     frontier_handle_duplicates = set()
-    frontier.put(Node(initial_state, []))
+    frontier.put(Node(initial, []))
     frontier_handle_duplicates.add(node.state)
 
     explored = set()
@@ -64,22 +74,53 @@ def DepthFirstSearch(problem: Problem[S, A], initial_state: S) -> Solution:
             return node.cumlative_successful_path
 
         for action in problem.get_actions(node.state):
-            child_state = problem.get_successor(node.state, action)
+            child = problem.get_successor(node.state, action)
 
-            if child_state not in explored and child_state not in frontier_handle_duplicates: 
-                frontier.put(Node(child_state, node.cumlative_successful_path + [action]))
-                frontier_handle_duplicates.add(child_state)
+            if child not in explored and child not in frontier_handle_duplicates: 
+                frontier.put(Node(child, node.cumlative_successful_path + [action]))
+                frontier_handle_duplicates.add(child)
 
     return None
     
-def UniformCostSearch(problem: Problem[S, A], initial_state: S) -> Solution:
+def UniformCostSearch(problem: Problem[S, A], initial: S) -> Solution:
+    #TODO: ADD YOUR CODE HERE
+    node=Node(initial,[],0)
+    frontier = queue.PriorityQueue()
+    frontier_handle_duplicates=set()
+    frontier.put(node)
+    #this set is added as we are implementing the graph version of the algorithm so we need to handle duplicates states that enters the frontier
+    frontier_handle_duplicates.add(node.state)
+    explored = set()
+    while not frontier.empty():
+        node = frontier.get()
+        explored.add(node.state)
+        if problem.is_goal(node.state):
+            return node.cumlative_successful_path
+        for action in problem.get_actions(node.state):
+            child = problem.get_successor(node.state, action)
+            child_path = node.cumlative_successful_path + [action]
+            child_cost = node.cost + problem.get_cost(node.state, action)
+            child_node = Node(child, child_path, child_cost)
+            
+            # Check if child is in explored or frontier
+            if child not in explored and child not in frontier_handle_duplicates:
+    
+                frontier.put(child_node)
+                frontier_handle_duplicates.add(child)
+                
+            elif child_node in frontier.queue:
+                #get frontier index
+                frontierPosition=frontier.queue.index(child_node)
+                frontier_top=frontier.queue[frontierPosition]
+                if child_cost<=frontier_top.cost:
+                    frontier.queue[frontierPosition]=child_node          
+    
+    return None  
+
+def AStarSearch(problem: Problem[S, A], initial: S, heuristic: HeuristicFunction) -> Solution:
     #TODO: ADD YOUR CODE HERE
     utils.NotImplemented()
 
-def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFunction) -> Solution:
-    #TODO: ADD YOUR CODE HERE
-    utils.NotImplemented()
-
-def BestFirstSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFunction) -> Solution:
+def BestFirstSearch(problem: Problem[S, A], initial: S, heuristic: HeuristicFunction) -> Solution:
     #TODO: ADD YOUR CODE HERE
     utils.NotImplemented()
