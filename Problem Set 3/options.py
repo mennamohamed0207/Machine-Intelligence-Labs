8da4fdf96ae4f77555a8ce8dc6bb214c3234db64
mennamohamed0207 <menamohamed0207@gmail.com>
2024-12-07 16:54:52 +0200

    Adding problem set 3

---
+++ Problem Set 3/options.py
@@ -0,0 +1,53 @@
+# This file contains the options that you should modify to solve Question 2
+
+# IMPORTANT NOTE:
+# Comment your code explaining why you chose the values you chose.
+# Uncommented code will be penalized.
+
+def question2_1():
+    #TODO: Choose options that would lead to the desired results 
+    return {
+        "noise": 0,
+        "discount_factor": 1,
+        "living_reward": 0
+    }
+
+def question2_2():
+    #TODO: Choose options that would lead to the desired results
+    return {
+        "noise": 0,
+        "discount_factor": 1,
+        "living_reward": 0
+    }
+
+def question2_3():
+    #TODO: Choose options that would lead to the desired results
+    return {
+        "noise": 0,
+        "discount_factor": 1,
+        "living_reward": 0
+    }
+
+def question2_4():
+    #TODO: Choose options that would lead to the desired results
+        return {
+        "noise": 0,
+        "discount_factor": 1,
+        "living_reward": 0
+    }
+
+def question2_5():
+    #TODO: Choose options that would lead to the desired results
+    return {
+        "noise": 0,
+        "discount_factor": 1,
+        "living_reward": 0
+    }
+
+def question2_6():
+    #TODO: Choose options that would lead to the desired results
+    return {
+        "noise": 0,
+        "discount_factor": 1,
+        "living_reward": 0
+    }
