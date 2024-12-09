736095c875f73b99ec29985091e74ea5ee916d2c
mennamohamed0207 <menamohamed0207@gmail.com>
2024-12-10 00:21:59 +0200

    solving partially 4/6 problem 2

--- Problem Set 3/options.py
+++ Problem Set 3/options.py
@@ -8,15 +8,15 @@ def question2_1():
     #TODO: Choose options that would lead to the desired results 
     return {
         "noise": 0,
-        "discount_factor": 1,
-        "living_reward": 0
+        "discount_factor": 0.2,
+        "living_reward":-0.2
     }
 
 def question2_2():
     #TODO: Choose options that would lead to the desired results
     return {
         "noise": 0,
-        "discount_factor": 1,
+        "discount_factor": 0.9,
         "living_reward": 0
     }
 
@@ -24,30 +24,30 @@ def question2_3():
     #TODO: Choose options that would lead to the desired results
     return {
         "noise": 0,
-        "discount_factor": 1,
-        "living_reward": 0
+        "discount_factor": 0.9,
+        "living_reward": -1
     }
 
 def question2_4():
     #TODO: Choose options that would lead to the desired results
         return {
         "noise": 0,
-        "discount_factor": 1,
-        "living_reward": 0
+        "discount_factor": 0.5,
+        "living_reward": -0.002
     }
 
 def question2_5():
     #TODO: Choose options that would lead to the desired results
     return {
         "noise": 0,
-        "discount_factor": 1,
-        "living_reward": 0
+        "discount_factor": 0.1,
+        "living_reward": 100
     }
 
 def question2_6():
     #TODO: Choose options that would lead to the desired results
     return {
         "noise": 0,
-        "discount_factor": 1,
-        "living_reward": 0
+        "discount_factor": 0.1,
+        "living_reward": -100
     }
