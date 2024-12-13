ba9e664b98c119fb1670a08dc84bffdfb4ceab14
mennamohamed0207 <menamohamed0207@gmail.com>
2024-12-14 01:20:52 +0200

    Adding problem 2

--- Problem Set 3/options.py
+++ Problem Set 3/options.py
@@ -15,9 +15,9 @@ def question2_1():
 def question2_2():
     #TODO: Choose options that would lead to the desired results
     return {
-        "noise": 0,
-        "discount_factor": 0.9,
-        "living_reward": 0
+        "noise": 0.1,
+        "discount_factor": 0.5,
+        "living_reward": -1
     }
 
 def question2_3():
@@ -31,9 +31,10 @@ def question2_3():
 def question2_4():
     #TODO: Choose options that would lead to the desired results
         return {
-        "noise": 0,
-        "discount_factor": 0.5,
-        "living_reward": -0.002
+        "noise": 0.2,
+        "discount_factor": 1,
+        "living_reward": -0.02
+    
     }
 
 def question2_5():
